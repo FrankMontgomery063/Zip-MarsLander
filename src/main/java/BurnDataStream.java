96ce09adc201281c9e594a0670c73751a738ffdb
FrankMontgomery063 <montgomeryfrank.fm@gmail.com>
2025-11-09 16:53:02 -0500

    BurnDataStream

--- src/main/java/BurnDataStream.java
+++ src/main/java/BurnDataStream.java
@@ -1,9 +1,11 @@
 public class BurnDataStream implements BurnStream {
     // these are the series of burns made each 10 secs by the lander.
     // change them to see if you can get the lander to make a soft landing.
-    // burns are between 0 and 200. This burn array usually crashes.
+    // burns are between 0 and 200. This burn array should achieve soft landing.
+    // Physics: DeltaV = 100 - Burn (0=+100 m/s, 100=0 m/s, 200=-100 m/s)
+    // Goal: Land at 1-2 m/s for success
 
-    int burnArray[] = {100, 100, 200, 200, 100, 100, 0, 0, 200, 100, 100, 0, 0, 0, 0};
+    int burnArray[] = {0, 0, 0, 0, 50, 75, 90, 95, 100, 105, 110, 120, 140, 180, 198};
     int burnIdx = -1;
 
     public BurnDataStream() { }
@@ -12,7 +14,7 @@ public class BurnDataStream implements BurnStream {
     }
     @Override
     public int getNextBurn(DescentEvent status) {
-        if (burnIdx < burnArray.length) {
+        if (burnIdx < burnArray.length - 1) {
             burnIdx++;
             System.out.println(burnArray[burnIdx]); /*hack!*/
             return burnArray[burnIdx];
